1c533504d2ba4ec4c29c35d1078f24918038286d
Soichiro-Yoshimura <yoshimura@soichiro.org>
2019-05-16 20:23:29 +0900

    Add join message functions.

--- scripts/chat.js
+++ scripts/chat.js
@@ -3,7 +3,32 @@
 const Goodcount = require('../models/goodcount');
 Goodcount.sync();
 
+const fs = require('fs');
+const joinMessagesFileName = './join_messages.json';
+let joinMessages = new Map(); // key: チャンネルID, value: 入室メッセージ
+
+function saveJoinMessages() {
+  fs.writeFileSync(
+    joinMessagesFileName,
+    JSON.stringify(Array.from(joinMessages)),
+    'utf8'
+  );
+}
+
+function loadJoinMessages() {
+  try {
+    const data = fs.readFileSync(joinMessagesFileName, 'utf8');
+    joinMessages = new Map(JSON.parse(data));
+  } catch (e) {
+    console.log('loadJoinMessages Error:');
+    console.log(e);
+    console.log('空のjoinMessagesを利用します');
+  }
+}
+
 module.exports = robot => {
+  loadJoinMessages();
+
   // :+1: が付くと名前付きで褒めてくれ、いいねの数をカウント
   const sentSet = new Set(); // 送信済みいいね (TS:sendUserId)
 
@@ -155,21 +180,45 @@ module.exports = robot => {
     msg.send(message);
   });
 
-  //部屋に入ったユーザーへの案内
+  // 発言したチャンネルに入室メッセージを設定する
+  robot.hear(/^入室メッセージを登録して (.*)/i, msg => {
+    const parsed = msg.message.rawText.match(/^入室メッセージを登録して (.*)/);
+    // console.log(msg);
+    if (parsed) {
+      const joinMessage = parsed[1];
+      const channelId = msg.envelope.room;
+      joinMessages.set(channelId, joinMessage);
+      saveJoinMessages();
+      msg.send(`入室メッセージ:「${joinMessage}」を登録したよ。`);
+    }
+  });
+
+  // 発言したチャンネルの入室メッセージの設定を解除する
+  robot.hear(/^入室メッセージを消して/i, msg => {
+    const channelId = msg.envelope.room;
+    joinMessages.delete(channelId);
+    saveJoinMessages();
+    msg.send(`入室メッセージを削除したよ。`);
+  });
+
+  //部屋に入ったユーザーへの入室メッセージを案内 %USERNAME% はユーザー名に、%ROOMNAME% は部屋名に置換
   robot.enter(msg => {
     let username = msg.message.user.profile.display_name;
     if (!username) username = msg.message.user.name;
 
     //チャンネルのIDからチャンネル名を取得
     const channelId = msg.envelope.room;
-    const room_name = robot.adapter.client.rtm.dataStore.getChannelGroupOrDMById(
+    const roomname = robot.adapter.client.rtm.dataStore.getChannelGroupOrDMById(
       channelId
     ).name;
 
-    if (channelId === 'C0VF1QBEK') { // #programming
-      const message = `${username}ちゃんだね！ ここは #${room_name} だよ！` + 
-       `このあたりのことなら https://n-highschool.slack.com/files/U0VKMCTEV/FHPNW4TEU/_programming_________________________ をみてね。`;
-      msg.send(message);
+    for (let [key, value] of joinMessages) {
+      if (channelId === key) {
+        let message = value
+          .replace('%USERNAME%', username)
+          .replace('%ROOMNAME%', '#' + roomname);
+        msg.send(message);
+      }
     }
   });
 };
