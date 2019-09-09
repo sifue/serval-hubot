fbffa05cb7b68967c82327487def9daad5aeb432
Soichiro Yoshimura <yoshimura@soichiro.org>
2019-09-09 18:51:45 +0900

    #を追加

--- scripts/report.js
+++ scripts/report.js
@@ -78,7 +78,9 @@ module.exports = robot => {
 
       channels.forEach(c => {
         attachment.fields.push({
-          value: c.is_new ? `${c.name} (新規)` : `${c.name}`,
+          value: c.is_new
+            ? `#${c.name} ${c.name} (新規)`
+            : `#${c.name} ${c.name}`,
           short: true
         });
 
